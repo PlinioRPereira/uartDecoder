0d839e3ba2e8075011b1409eefe697aa608bdee3
Luiz Luz <luizluz@dcc.ufmg.br>
2023-12-20 19:08:29 -0300

    Atualizando scripts de experimentos

--- experimentos/captureAudio.py
+++ experimentos/captureAudio.py
@@ -8,7 +8,7 @@ DTYPE = np.int16 # Tipo de dado
 CHANNELS = 2    # Número de canais
 SECONDS = 5     # Duração
 DEVICE_ID = 17   # ID do dispositivo de entrada
-FILENAME = "output.wav"
+FILENAME = "Audio2min_23a25hz.wav"
 
 # Iniciar a gravação
 print("Gravando...")
--- experimentos/generateAudio.py
+++ experimentos/generateAudio.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sounddevice as sd
-import time
+from scipy.io.wavfile import write
 
 # Frequências fundamentais para as notas da oitava 4
 frequencies = {
@@ -11,32 +11,41 @@ frequencies = {
     "sol": 392.00,
     "la": 440.00,
     "si": 493.88,
+    "23hz": 23/0.07,
+    "24hz": 24/0.07,
+    "25hz": 25/0.07,
 }
 
-freqAdjustFactor = 1/10
-
-def generate_note(note, octave, duration=1.0, sample_rate=44100):
-    freq = frequencies[note.lower()] * (2 ** (octave - 4))/freqAdjustFactor
+def generate_note(note, octave, duration=1.0, sample_rate=44100, adjust_factor=0.07):
+    freq = frequencies[note.lower()] * (2 ** (octave - 4)) * adjust_factor
     t = np.linspace(0, duration, int(sample_rate * duration), endpoint=False)
-    audio_data = 0.5 * np.sin(2 * np.pi * freq * t)
+    audio_data = np.sin(2 * np.pi * freq * t)
     return audio_data
 
-def play_notes(notes, octaves, duration=500.0, sample_rate=44100):
+def save_wav(filename, data, sample_rate):
+    # Ajuste a amplitude antes de converter para int16
+    data = np.int16(data / np.max(np.abs(data)) * 32767)
+    write(filename, sample_rate, data)
+
+def play_notes(notes, octaves, duration=180.0, sample_rate=44100):
     audio_data = np.zeros(int(sample_rate * duration))
 
     for note in notes:
         for octave in octaves:
             audio_data += generate_note(note, octave, duration, sample_rate)
 
-    # Normaliza o áudio para evitar clipping
-    audio_data = audio_data / (len(notes) * len(octaves))
+    # Salva o arquivo WAV
+    save_wav("Audio2min_23a25hz.wav", audio_data, sample_rate)
 
+    # Toca o áudio
     sd.play(audio_data, samplerate=sample_rate)
     sd.wait()
 
 # Todas as 7 notas e 8 oitavas
 all_notes = ["do", "re", "mi", "fa", "sol", "la", "si"]
-all_octaves = [1, 2, 3, 4, 5, 6, 7, 8]
+all_octaves = [3, 4, 5]
+# all_notes = ["24hz"]
+# all_octaves = [1,2,3,4,5,6,7]
 
 # Toca todas as notas e oitavas simultaneamente
 play_notes(all_notes, all_octaves)
